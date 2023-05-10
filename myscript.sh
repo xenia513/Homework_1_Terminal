d7e1cc5022d46eae14ebe53242dd5299e78057a2
Kseniya Kiseleva <109612371+xenia513@users.noreply.github.com>
2023-05-10 13:43:16 +0400

    Add script

---
+++ myscript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+/c/Users/User/desktop
+
+cd homework_1 # Зайти в папку
+
+mkdir test1 test2 test3 # Создать 3 папки
+
+cd test1 # Зайти в любую папку
+
+touch tst1.txt tst2.txt tst3.txt json1.json json2.json # Создать 5 файлов 
+
+mkdir test_1 test_2 test_3 # Создать 3 папки
+
+ls -la # вывести список содержимого папки
+
+mv {tst1.txt,tst2.txt} test_2 # переместить 2 файла в другую папку
